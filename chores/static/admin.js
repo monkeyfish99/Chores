360281516dade827defd14704011d7371b99bd97
Monkeyfish <monkeyfish@monkey.fish>
2017-08-29 20:16:44 -0700

    Remove unused javascript file
    Was an attempt at something, didn't work out.

--- chores/static/admin.js
+++
@@ -1,8 +0,0 @@
-var check = function() {
-    if (document.getElementById("password").value == document.getElementyById("confirm").value) {
-        document.getElementById("message").innerHTML = "";
-    } else {
-        document.getElementById("message").innerHTML = "Passwords must match";
-        document.getElementById("message").style.color = "red";
-    }
-}
